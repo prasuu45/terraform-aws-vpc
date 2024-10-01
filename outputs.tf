06dea3a1515736db79e87f137352edb3c851fa74
prasuu45 <prasuuchow93@gmail.com>
2024-10-01 11:58:15 +0530

    terraform

--- outputs.tf
+++ outputs.tf
@@ -15,9 +15,11 @@ output "default_vpc_info" {
 output "public_subnet_ids"{
   value = aws_subnet.public[*].id
 }
+
 output "private_subnet_ids"{
   value = aws_subnet.private[*].id
 }
+
 output "database_subnet_ids"{
   value = aws_subnet.database[*].id
 }
