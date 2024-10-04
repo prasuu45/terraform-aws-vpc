319214ee1a505cf8070f64be8052c1b0f296066c
prasuu45 <prasuuchow93@gmail.com>
2024-10-04 15:31:38 +0530

    terraform

--- outputs.tf
+++ outputs.tf
@@ -23,7 +23,8 @@ output "private_subnet_ids"{
 output "database_subnet_ids"{
   value = aws_subnet.database[*].id
 }
-
+ 
+ 
 
 output "database_subnet_group_name"{
   value = aws_db_subnet_group.default.name
