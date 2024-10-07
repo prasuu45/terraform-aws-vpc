42dcbd908e0c8ce6f5d13d7b2f539565783bbc69
prasuu45 <prasuuchow93@gmail.com>
2024-10-07 12:08:38 +0530

    terraform

--- main.tf
+++ main.tf
@@ -157,19 +157,19 @@ resource "aws_route_table" "database" {
 
 resource "aws_route" "public" {
   route_table_id            = aws_route_table.public.id
-  destination_cidr_block    = "0.0.0.0/16"
+  destination_cidr_block    = "0.0.0.0/0"
   gateway_id = aws_internet_gateway.igw.id
 }
 
 resource "aws_route" "private" {
   route_table_id            = aws_route_table.privat.id
-  destination_cidr_block    = "0.0.0.0/16"
+  destination_cidr_block    = "0.0.0.0/0"
   nat_gateway_id = aws_nat_gateway.main.id
 }
 
 resource "aws_route" "database" {
   route_table_id            = aws_route_table.database.id
-  destination_cidr_block    = "0.0.0.0/16"
+  destination_cidr_block    = "0.0.0.0/0"
   nat_gateway_id = aws_nat_gateway.main.id
 }
 #route_table_assciation
